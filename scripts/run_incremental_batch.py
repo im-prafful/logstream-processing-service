71d47fe50c3259bcf6c988d79531847e02cf964c
Prafful <imprafful26@gmail.com>
2026-01-07 23:41:26 +0530

    updated

--- scripts/run_incremental_batch.py
+++ scripts/run_incremental_batch.py
@@ -4,12 +4,20 @@ import os
 
 sys.path.append(sys.path[0] + "/..")
 
-from src.db_connector import detect_and_create_incidents, get_db_engine, fetch_logs_batch, save_embedding, save_pattern, fetch_min_timestamp
+from src.db_connector import (
+    detect_and_create_incidents,
+    get_db_engine,
+    fetch_logs_batch,
+    save_embedding,
+    save_pattern,
+    fetch_min_timestamp,
+)
 from src.vector_engine import SemanticVectorEngine
 from src.pipeline import get_text_embedding, build_feature_dict
 from src.model import load_model
 
 PRODUCTION_DIR = "models/production"
+
 
 def main():
     print("--- STARTING INCREMENTAL INFERENCE (BLUE Deployment) ---")
@@ -29,8 +37,6 @@ def main():
 
     engine = get_db_engine()
 
-    
-    
     # 2. PROCESS NEW LOGS (Read-Only Inference)
     query = """
         SELECT * FROM logs 
@@ -39,16 +45,19 @@ def main():
         LIMIT 2000;
     """
 
-    timestamp_query = text("""
+    timestamp_query = text(
+        """
         SELECT MIN(timestamp) 
         FROM logs 
         WHERE level IN ('error','warning') 
         AND cluster_id IS NULL
-    """)
+    """
+    )
 
     df_new = fetch_logs_batch(engine, query)
-    global_timestamp = fetch_min_timestamp(engine=engine, timestamp_query=timestamp_query)
-
+    global_timestamp = fetch_min_timestamp(
+        engine=engine, timestamp_query=timestamp_query
+    )
 
     if df_new.empty:
         print("No new logs.")
@@ -80,9 +89,10 @@ def main():
 
     save_pattern(engine=engine)
 
+    detect_and_create_incidents(
+        engine=engine, batch_size=batch_size, global_timestamp=global_timestamp
+    )  # Pass batch_size
 
-    detect_and_create_incidents(engine=engine, batch_size=batch_size, global_timestamp=global_timestamp)  # Pass batch_size
-        
     print("Inference batch complete.")
 
 
